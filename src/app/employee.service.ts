734dcde009ca49eee5a5b955a014811df71febf7
Mogeeswaran <mogeeswaran1993@gmai.com>
2020-08-21 23:04:15 +0530

    build

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private baseUrl = 'http://localhost:8080/springboot-crud-rest/api/v1/employees';
+  // private baseUrl = 'http://localhost:8080/springboot-crud-rest/api/v1/employees';
+  private baseUrl = `${environment.basepath}/springboot-crud-rest/api/v1/employees`;
 
   constructor(private http: HttpClient) { }
 
